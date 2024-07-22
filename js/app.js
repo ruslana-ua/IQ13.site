dbe7a575550d2e2c02f1bfb1623cded8d874ea47
Your Name <you@example.com>
2024-07-22 13:16:03 +0300

    new 15 pages All female services

--- js/app.js
+++ js/app.js
@@ -4087,13 +4087,6 @@
                 document.documentElement.classList.add(className);
             }));
         }
-        function addLoadedClass() {
-            if (!document.documentElement.classList.contains("loading")) window.addEventListener("load", (function() {
-                setTimeout((function() {
-                    document.documentElement.classList.add("loaded");
-                }), 0);
-            }));
-        }
         let _slideUp = (target, duration = 500, showmore = 0) => {
             if (!target.classList.contains("_slide")) {
                 target.classList.add("_slide");
@@ -10026,7 +10019,6 @@
         }));
         window["FLS"] = false;
         isWebp();
-        addLoadedClass();
         menuInit();
         spollers();
         formFieldsInit({
